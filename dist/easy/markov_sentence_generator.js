fa9b3ba4c5541cf8c9ddf015f681bfd2fd0b8231
Vincent Callede <vincent.callededev@gmail.com>
2024-09-24 22:48:06 +0200

    initial commit

---
+++ dist/easy/markov_sentence_generator.js
@@ -0,0 +1,4 @@
+"use strict";
+let arr = ["1", "2"];
+let arr2 = [arr, arr, arr, arr];
+console.log(arr2);
---
+++ easy/markov_sentence_generator.ts
@@ -0,0 +1,20 @@
+
+// INPUT = TEXT
+
+
+
+
+
+
+// OUTPUT = First (random pair) then random word that commes after each pair
+
+
+
+
+let arr = ["1","2"];
+let arr2 = [arr, arr, arr, arr];
+
+
+console.log(arr2);
+
+
