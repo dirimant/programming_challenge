6fa67aedd96c3ee12efe1c500f201fdf4f4ab059
Vincent Callede <vincent.callededev@gmail.com>
2024-09-29 13:21:29 +0200

    FizzBuzz implementation

---
+++ easy/fizzbuzz.ts
@@ -0,0 +1,25 @@
+//Given an integer n, return a string array answer (1-indexed) where:
+//    answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+//    answer[i] == "Fizz" if i is divisible by 3.
+//    answer[i] == "Buzz" if i is divisible by 5.
+//    answer[i] == i (as a string) if none of the above conditions are true.
+
+function fizzBuzz(value: number){
+    let numbers = new Map<number,any>()
+    for(let i = 1; i<value; i++){
+        numbers.set(i,i);
+    }
+    
+    for(let i = 1; i < numbers.size; i++){
+        if(numbers.get(i) % 3 == 0) numbers.set(i, 'Fizz');
+        if(numbers.get(i) % 5 == 0) numbers.set(i, 'Buzz');
+        if(numbers.get(i) % 3 == 0 && numbers.get(i) % 5 == 0) numbers.set(i, 'FizzBuzz');
+    }
+    return numbers;
+}
+
+function main(){
+    console.log(fizzBuzz(10));
+}
+
+main();
